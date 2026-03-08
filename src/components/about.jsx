a7e3dc6d2163a03bdcb2b1a48c982a05affb280b
CodeOmari <jacksonomari2230@gmail.com>
2026-03-09 00:18:19 +0300

    add project about section file

---
+++ src/components/about.jsx
@@ -0,0 +1,21 @@
+import '../styles/about.css';
+export default function About(){
+    return(
+        <div className="container about-section" id='about'>
+            <h3 className='mb-4'>ABOUT</h3>
+
+            <p>
+                I'm a Computer Science student and passionate full-stack developer, dedicated to 
+                turning ideas into functional, <br /> user-friendly web applications. <br />
+                <br />
+                My journey began with HTML & CSS, evolved through JavaScript and React on the frontend, and expanded into <br /> Python 
+                and Django on the backend. I thrive on building projects that solve real problems, exploring 
+                new technologies, <br />and contributing to the open-source community. <br />
+                <br />
+                When I'm not coding, you'll often find me tackling algorithmic challenges or learning the latest trends in web development. <br />
+                <br />
+                I aim to craft seamless digital experiences that combine functionality, aesthetics, and innovation.
+            </p>
+        </div>
+    )
+}
