212d11af7e590d17ac56a3fe2acfaafeeeb1cf90
CodeOmari <jacksonomari2230@gmail.com>
2026-03-09 01:02:52 +0300

    update image referencing in the contact section

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,8 @@
 import "../styles/contact.css";
+import Email from "../assets/email.svg";
+import Github from "../styles/github.svg";
+import Linkedin from "../styles/linkedin.svg";
+import Gmail from "../styles/gmail.svg";
 export default function Contact(){
     return(
         <div className="container-fluid contact-section bg-light d-flex flex-column justify-content-center align-items-center" id="contact">
@@ -15,7 +19,7 @@ export default function Contact(){
             </div>
             <div className="mail mb-5">
                 <a href="mailto:jacksonomari2230@gmail.com" target="_blank" className="p-3 rounded-pill">
-                    <img src="../src/assets/email.svg" alt="envelope icon" className="pe-2" />
+                    <img src={Email} alt="envelope icon" className="pe-2" />
                     Say Hello
                 </a>
             </div>
@@ -23,17 +27,17 @@ export default function Contact(){
                 <div className="row">
                     <div className="col-sm-4">
                         <a href="https://github.com/CodeOmari" target="_blank">
-                            <img src="../src/assets/github.svg" alt="github logo" className="w-50" />
+                            <img src={Github} alt="github logo" className="w-50" />
                         </a>
                     </div>
                     <div className="col-sm-4">
                         <a href="https://www.linkedin.com/in/jackson-omari/" target="_blank">
-                            <img src="../src/assets/linkedin.svg" alt="github logo" className="w-50" />
+                            <img src={Linkedin} alt="linkedin logo" className="w-50" />
                         </a>
                     </div>
                     <div className="col-sm-4">
                         <a href="mailto:jacksonomari2230@gmail.com" target="_blank">
-                            <img src="../src/assets/gmail.svg" alt="github logo" className="w-50" />
+                            <img src={Gmail} alt="gmail logo" className="w-50" />
                         </a>
                     </div>
                 </div>
