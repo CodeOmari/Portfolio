e69059b7a3aac7d7a69e446ee2e74b6caa1b2aaf
CodeOmari <jacksonomari2230@gmail.com>
2026-03-09 22:51:44 +0300

    update social media icons to be responsive on mobile screen sizes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -23,24 +23,18 @@ export default function Contact(){
                     Say Hello
                 </a>
             </div>
-            <div className="container social-media">
-                <div className="row">
-                    <div className="col-12 col-sm-4">
-                        <a href="https://github.com/CodeOmari" target="_blank">
-                            <img src={Github} alt="github logo" className="icon" />
-                        </a>
-                    </div>
-                    <div className="col-12 col-sm-4">
-                        <a href="https://www.linkedin.com/in/jackson-omari/" target="_blank">
-                            <img src={Linkedin} alt="linkedin logo" className="icon" />
-                        </a>
-                    </div>
-                    <div className="col-12 col-sm-4">
-                        <a href="mailto:jacksonomari2230@gmail.com" target="_blank">
-                            <img src={Gmail} alt="gmail logo" className="icon" />
-                        </a>
-                    </div>
-                </div>
+            <div className="social-media d-flex justify-content-center gap-4">
+                <a href="https://github.com/CodeOmari" target="_blank">
+                    <img src={Github} alt="github logo" className="icon" />
+                </a>
+
+                <a href="https://www.linkedin.com/in/jackson-omari/" target="_blank">
+                    <img src={Linkedin} alt="linkedin logo" className="icon" />
+                </a>
+
+                <a href="mailto:jacksonomari2230@gmail.com" target="_blank">
+                    <img src={Gmail} alt="gmail logo" className="icon" />
+                </a>
             </div>
         </div>
     )
