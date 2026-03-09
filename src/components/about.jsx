f66acf249cc0be3099b49899d263f3c9c6dcef97
CodeOmari <jacksonomari2230@gmail.com>
2026-03-09 22:30:06 +0300

    update the about section to be responsive on small  screen sizes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,11 +6,11 @@ export default function About(){
 
             <p>
                 I'm a Computer Science student and passionate full-stack developer, dedicated to 
-                turning ideas into functional, <br /> user-friendly web applications. <br />
+                turning ideas into functional, user-friendly web applications. <br />
                 <br />
                 My journey began with HTML & CSS, evolved through JavaScript and React on the frontend, and expanded into <br /> Python 
                 and Django on the backend. I thrive on building projects that solve real problems, exploring 
-                new technologies, <br />and contributing to the open-source community. <br />
+                new technologies, and contributing to the open-source community. <br />
                 <br />
                 When I'm not coding, you'll often find me tackling algorithmic challenges or learning the latest trends in web development. <br />
                 <br />
