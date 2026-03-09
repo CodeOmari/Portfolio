137d5e171ec68c893b4d08cf1628d2b8db934d25
CodeOmari <jacksonomari2230@gmail.com>
2026-03-09 23:22:16 +0300

    update footer section to include my phone number

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
     return(
         <div className="container-fluid pt-4 footer-section d-flex justify-content-center bg-light">
             <p>
-                All Rights reserved &copy;{currentYear}.
+                All Rights reserved &copy;{currentYear}. | Contact: +254715895954
             </p>    
         </div>
     )
