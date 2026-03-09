9f67b0247e12fd0d8f3ac71835d9caf00ea1b1e1
CodeOmari <jacksonomari2230@gmail.com>
2026-03-09 21:56:33 +0300

    update navigation bar to a collpase on mboilde devices

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,17 +2,16 @@ import "../styles/navigation.css";
 
 export default function NavBar(){
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light sticky-top p-3 navigation-section">
-            <div className="container-fluid">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light sticky-top p-3 navigation-section">
+            <div class="container-fluid">
                 <div className="name">
                     <h4>Jackson Omari</h4>
                 </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-                <span className="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav ms-auto">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
                             <a className="nav-link rounded-pill p-2" href="#about">About</a>
                             </li>
@@ -25,8 +24,8 @@ export default function NavBar(){
                             <li className="nav-item">
                             <a className="nav-link rounded-pill p-2" href="#contact">Contact</a>
                             </li>
-                        </ul>
-                    </div>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
