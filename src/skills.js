052d40e51b45270242eef6353e62242792d5ca2c
CodeOmari <jacksonomari2230@gmail.com>
2026-04-30 22:09:14 +0300

    add skills data file for dynamic rendering

---
+++ src/skills.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: "HTML"
+    },
+    {
+        name: "CSS"
+    },
+    {
+        name: "Bootstrap"
+    },
+    {
+        name: "JavaScript"
+    },
+    {
+        name: "React"
+    },
+    {
+        name: "Python"
+    },
+    {
+        name: "Django"
+    },
+    {
+        name: "REST APIs"
+    },
+    {
+        name: "MySQL"
+    },
+    {
+        name: "Git"
+    },
+    {
+        name: "Github"
+    },
+    {
+        name: "Postman"
+    },
+]
