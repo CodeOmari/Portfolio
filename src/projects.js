b6000ca6a9c380d8b2d66d401ef4d889fd495702
CodeOmari <jacksonomari2230@gmail.com>
2026-04-30 21:58:19 +0300

    add file containing data for the projects section

---
+++ src/projects.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        link: "https://note-nest-bird.vercel.app/",
+        title: "NoteNest",
+        description: "A simple notes app built with React, Bootstrap, Django Rest Framework, and MySQL for creating, organizing, and managing personal notes.",
+        technologies: ["React", "Bootstrap", "REST APIs", "MySQL"]
+    },
+    {
+        link: "https://omari14.pythonanywhere.com/",
+        title: "NovaCare",
+        description: "A hospital management system built with HTML, CSS, Bootstrap, and Django, using MySQL as the database to manage hospital services and patient information efficiently.",
+        technologies: ["HTML", "CSS", "Bootstrap", "Django", "MySQL"]
+    },
+    {
+        link: "https://talkio-gray.vercel.app/",
+        title: "Talkio",
+        description: "A lightweight messaging app built with React, Bootstrap, Django Rest Framework, and MySQL designed to make connecting and chatting with people simple, fast, and seamless.",
+        technologies: ["React", "Bootstrap", "REST APIs", "MySQL"]
+    },
+    {
+        link: "https://codeomari.pythonanywhere.com/",
+        title: "Eventify",
+        description: "A events management platform that allows users to discover, register, and manage educational, tech, business, and art events, as well as workshops. Users can explore both free and paid events, whether physical or virtual, all in one place.",
+        technologies: ["HTML", "CSS", "Bootstrap", "Django", "MySQL"]
+    },
+]
