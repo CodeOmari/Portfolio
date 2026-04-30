113227bc85648ee71928f527aa701d3c436ab8c9
CodeOmari <jacksonomari2230@gmail.com>
2026-04-30 21:58:44 +0300

    update projects section to map data from a json file

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
 import '../styles/projects.css';
+import projectData from "../projects";
 
-export default function Projects() {
+export default function Projects(){
     return(
         <div className="container-fluid projects-section" id='projects'>
             <div className="container">
@@ -9,79 +10,24 @@ export default function Projects() {
                     My Projects
                 </h5>
             </div>
-            <div className="container project-container p-4">
-                <div className="project-name">
-                    <a href="https://note-nest-bird.vercel.app/" target='_blank'>NoteNest</a>
-                </div>
-                <div className="project-description">
-                    <p>
-                        A simple notes app built with React, Bootstrap, Django Rest Framework, and MySQL 
-                        for creating, organizing, and managing personal notes.
-                    </p>
-                    <div className="lang-box d-flex">
-                        <button className='rounded-pill language'>React</button>
-                        <button className='rounded-pill language'>Bootstrap</button>
-                        <button className='rounded-pill language'>REST APIs</button>
-                        <button className='rounded-pill language'>MySQL</button>
+            {projectData.map((project) => (
+                <div className="container project-container p-4">
+                    <div className="project-name">
+                        <a href={project.link} target='_blank'>{project.name}</a>
+                    </div>
+                    <div className="project-description">
+                        <p>{project.description}</p>
+
+                        <div className="lang-box d-flex">
+                            {project.technologies.map((tech, index) => (
+                                <button key={index} className="rounded-pill language">
+                                    {tech}
+                                </button>
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="container project-container p-4">
-                <div className="project-name">
-                    <a href="https://omari14.pythonanywhere.com/" target='_blank'>NovaCare</a>
-                </div>
-                <div className="project-description">
-                    <p>
-                        A hospital management system built with HTML, CSS, Bootstrap, 
-                        and Django, using MySQL as the database to manage hospital services 
-                        and patient information efficiently.
-                    </p>
-                    <div className="lang-box d-flex">
-                        <button className='rounded-pill language'>HTML</button>
-                        <button className='rounded-pill language'>CSS</button>
-                        <button className='rounded-pill language'>Bootstrap</button>
-                        <button className='rounded-pill language'>Django</button>
-                        <button className='rounded-pill language'>MySQL</button>
-                    </div>
-                </div>
-            </div>
-            <div className="container project-container p-4">
-                <div className="project-name">
-                    <a href="https://talkio-gray.vercel.app/" target='_blank'>Talkio</a>
-                </div>
-                <div className="project-description">
-                    <p>
-                        A lightweight messaging app built with React, Bootstrap, Django Rest Framework, and MySQL 
-                        designed to make connecting and chatting with people simple, fast, and seamless.
-                    </p>
-                    <div className="lang-box d-flex">
-                        <button className='rounded-pill language'>React</button>
-                        <button className='rounded-pill language'>Bootstrap</button>
-                        <button className='rounded-pill language'>REST APIs</button>
-                        <button className='rounded-pill language'>MySQL</button>
-                    </div>
-                </div>
-            </div>
-            <div className="container project-container p-4">
-                <div className="project-name">
-                    <a href="https://codeomari.pythonanywhere.com/" target='_blank'>Eventify</a>
-                </div>
-                <div className="project-description">
-                    <p>
-                        A events management platform that allows users to discover, register, and 
-                        manage educational, tech, business, and art events, as well as workshops. 
-                        Users can explore both free and paid events, whether physical or virtual, all 
-                        in one place.
-                    </p>
-                    <div className="lang-box d-flex">
-                        <button className='rounded-pill language'>HTML</button>
-                        <button className='rounded-pill language'>CSS</button>
-                        <button className='rounded-pill language'>Bootstrap</button>
-                        <button className='rounded-pill language'>Django</button>
-                        <button className='rounded-pill language'>MySQL</button>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
